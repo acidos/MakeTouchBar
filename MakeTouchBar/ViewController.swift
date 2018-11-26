8e0910460eb701d3e733af4128764312c328ea1c
Suren Abeghyan <souren@gmail.com>
2018-11-26 09:47:20 +0100

    Initial Commit

---
+++ MakeTouchBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MakeTouchBar
+//
+//  Created by Suren Abeghyan on 26.11.18.
+//  Copyright © 2018 BlaBla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
